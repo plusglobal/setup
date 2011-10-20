eb683dd9095dc94b44a3f1826f907d039180cb4f
Mauro Zadunaisky <maurozadu@gmail.com>
2011-10-19 21:53:23 -0300

    actualizado a cake 1.3.13

--- public_html/index.php
+++ public_html/index.php
@@ -50,7 +50,7 @@
  *
  */
 	if (!defined('CAKE_CORE_INCLUDE_PATH')) {
-		define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'cake' . DS . '1.3.12');
+		define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'cake' . DS . '1.3.13');
 	}
 
 /**
